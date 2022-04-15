4ed0bd38da423061044dfa5b60dc541887972cb6
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2022-04-15 08:56:46 +0530

    WIP

--- app/Providers/MailConfigServiceProvider.php
+++ app/Providers/MailConfigServiceProvider.php
@@ -33,14 +33,36 @@ class MailConfigServiceProvider extends ServiceProvider
                     'port'       => $setting->port,
                     'encryption' => $setting->mail_encryption,
                     'username'   => $setting->user_name,
-                    'password'   => $setting->password
+                    'password'   => $setting->password,
+                    'timeout' => null,
+                    'auth_mode' => null,
                   );
                   $fromConfig = array('address' => $setting->from_email, 'name' =>  env('MAIL_FROM_NAME', 'QPM CONSTRUCTION') );
 
                   Config::set('mail.mailers.smtp', $config);
                   Config::set('mail.from', $fromConfig);
             }
+            else{
+                
+                $config =  [
+                        'transport' => 'smtp',
+                        'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+                        'port' => env('MAIL_PORT', 587),
+                        'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+                        'username' => env('MAIL_USERNAME'),
+                        'password' => env('MAIL_PASSWORD'),
+                        'timeout' => null,
+                        'auth_mode' => null,
+                    ];
 
-            dd($setting);
+                  $fromConfig =  [
+                            'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+                            'name' => env('MAIL_FROM_NAME', 'Example'),
+                        ];
+
+                  Config::set('mail.mailers.smtp', $config);
+                  Config::set('mail.from', $fromConfig);
+
+            }
     }
 }
