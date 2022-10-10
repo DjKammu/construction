2a9d50d6d2210da22b1a89d79d322bdd550ee8dc
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2022-10-10 13:36:04 +0530

    Updates

--- database/migrations/2022_10_09_115832_rename_column_in_proposals.php
+++ database/migrations/2022_10_09_115832_rename_column_in_proposals.php
@@ -16,6 +16,9 @@ class RenameColumnInProposals extends Migration
         Schema::table('f_f_e_proposals', function (Blueprint $table) {
              $table->dropConstrainedForeignId('subcontractor_id');
              $table->dropConstrainedForeignId('trade_id');
+              $table->dropColumn('f_f_e_vendor_id');
+              $table->dropColumn('f_f_e_trade_id');
+
              $table->unsignedBigInteger('f_f_e_vendor_id');
              $table->unsignedBigInteger('f_f_e_trade_id');
              $table->foreign('f_f_e_vendor_id')->references('id')->on('f_f_e_vendors')->onDelete('cascade');
