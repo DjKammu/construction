0507a304fe1ce1dd09213e8710c286748fd814c2
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2022-10-10 13:42:54 +0530

    Updates

--- database/migrations/2022_10_09_115832_rename_column_in_proposals.php
+++ database/migrations/2022_10_09_115832_rename_column_in_proposals.php
@@ -14,15 +14,15 @@ class RenameColumnInProposals extends Migration
     public function up()
     {
         Schema::table('f_f_e_proposals', function (Blueprint $table) {
-             $table->dropConstrainedForeignId('subcontractor_id');
-             $table->dropConstrainedForeignId('trade_id');
+             // $table->dropConstrainedForeignId('subcontractor_id');
+             // $table->dropConstrainedForeignId('trade_id');
               $table->dropColumn('f_f_e_vendor_id');
               $table->dropColumn('f_f_e_trade_id');
 
-             $table->unsignedBigInteger('f_f_e_vendor_id');
-             $table->unsignedBigInteger('f_f_e_trade_id');
-             $table->foreign('f_f_e_vendor_id')->references('id')->on('f_f_e_vendors')->onDelete('cascade');
-             $table->foreign('f_f_e_trade_id')->references('id')->on('f_f_e_trades')->onDelete('cascade');
+             // $table->unsignedBigInteger('f_f_e_vendor_id');
+             // $table->unsignedBigInteger('f_f_e_trade_id');
+             // $table->foreign('f_f_e_vendor_id')->references('id')->on('f_f_e_vendors')->onDelete('cascade');
+             // $table->foreign('f_f_e_trade_id')->references('id')->on('f_f_e_trades')->onDelete('cascade');
         });
     }
 
@@ -34,13 +34,13 @@ class RenameColumnInProposals extends Migration
     public function down()
     {
         Schema::table('f_f_e_proposals', function (Blueprint $table) {
-            $table->dropConstrainedForeignId('f_f_e_vendor_id');
-            $table->dropConstrainedForeignId('f_f_e_trade_id');
-            $table->unsignedBigInteger('subcontractor_id');
-            $table->foreign('subcontractor_id')->references('id')->on('subcontractors')
-            ->onDelete('cascade');
-            $table->unsignedBigInteger('trade_id');
-            $table->foreign('trade_id')->references('id')->on('trades')->onDelete('cascade');
+            // $table->dropConstrainedForeignId('f_f_e_vendor_id');
+            // $table->dropConstrainedForeignId('f_f_e_trade_id');
+            // $table->unsignedBigInteger('subcontractor_id');
+            // $table->foreign('subcontractor_id')->references('id')->on('subcontractors')
+            // ->onDelete('cascade');
+            // $table->unsignedBigInteger('trade_id');
+            // $table->foreign('trade_id')->references('id')->on('trades')->onDelete('cascade');
         });
     }
 }
---
+++ database/migrations/2022_10_10_081209_add_columns_in_f_f_e_proposals.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsInFFEProposals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('f_f_e_proposals', function (Blueprint $table) {
+             $table->unsignedBigInteger('f_f_e_vendor_id');
+             $table->unsignedBigInteger('f_f_e_trade_id');
+             $table->foreign('f_f_e_vendor_id')->references('id')->on('f_f_e_vendors')->onDelete('cascade');
+             $table->foreign('f_f_e_trade_id')->references('id')->on('f_f_e_trades')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('f_f_e_proposals', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('f_f_e_vendor_id');
+             $table->dropConstrainedForeignId('f_f_e_trade_id');
+        });
+    }
+}
