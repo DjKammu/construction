d0e8b553be651c32c36580a0b72946177d8bd552
Kamlesh <dhundharakamlesh@gmail.com>
2022-10-22 13:39:01 +0530

    WIP

--- resources/views/projects/ffe/payments-edit.blade.php
+++ resources/views/projects/ffe/payments-edit.blade.php
@@ -65,7 +65,7 @@
                                             <div class="form-group">
                                                 <label class="text-dark" for="password"> Trades
                                                 </label>
-                                                <select onchange="return window.location.href ='?trade='+this.value" class="form-control" name="f_f_e_trade_id"> 
+                                                <select class="form-control" name="f_f_e_trade_id"> 
                                                   @foreach($trades as $trade)
                                                    <option value="{{ $trade->id }}" {{ 
                                                    @$payment->proposal->f_f_e_trade_id == $trade->id ? 'selected' : '' 
