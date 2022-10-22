5566a880162280c79562db03ddfe1a7fb96ce9d9
Kamlesh <dhundharakamlesh@gmail.com>
2022-10-22 15:48:02 +0530

    WIP

---
+++ app/Http/Controllers/InspectionTypeController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\InspectionType;
+use App\Models\Document;
+use Gate;
+
+
+class InspectionTypeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         if(Gate::denies('view')) {
+               return abort('401');
+         } 
+
+         $inspetionTypes = InspectionType::orderBy('account_number');
+
+         $inspetionTypes = $inspetionTypes->paginate((new InspectionType())->perPage); 
+         
+         return view('inspection_types.index',compact('inspetionTypes'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        if(Gate::denies('add')) {
+               return abort('401');
+         } 
+
+        return view('inspection_types.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+          if(Gate::denies('add')) {
+               return abort('401');
+        } 
+
+        $data = $request->except('_token');
+
+        $request->validate([
+              'name' => 'required|unique:inspection_types',
+              'account_number' => 'required|unique:inspection_types',
+        ]);
+
+        $data['slug'] = \Str::slug($request->name);
+            
+        InspectionType::create($data);
+        return redirect('inspection-types')->with('message', 'Inspection Type Created Successfully!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+          if(Gate::denies('edit')) {
+               return abort('401');
+          } 
+
+         $type = InspectionType::find($id);
+         return view('inspection_types.edit',compact('type'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if(Gate::denies('update')) {
+               return abort('401');
+        } 
+
+        $data = $request->except('_token');
+
+        $request->validate([
+              'name' => 'required|unique:inspection_types,name,'.$id,
+              'account_number' => 'required|unique:inspection_types,account_number,'.$id,
+        ]);
+
+        $data['slug'] = \Str::slug($request->name);
+         
+         $type = InspectionType::find($id);
+         $slug = $data['slug'];
+         $oldSlug = $type->slug;
+        
+         if(!$type){
+            return redirect()->back();
+         }
+         
+
+         $type->update($data);
+
+        return redirect('inspection-types')->with('message', 'Inspection Type Updated Successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+         if(Gate::denies('delete')) {
+               return abort('401');
+          } 
+
+         $inspetionType = InspectionType::find($id);
+         $inspetionType->delete();       
+
+        return redirect()->back()->with('message', 'Inspection Type Successfully!');
+    }
+}
---
+++ resources/views/inspection_types/index.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.admin-app')
+
+@section('title', 'Inspection Type')
+
+@section('content')
+      <!-- Start Main View -->
+  <div class="card p-2">
+    <div class="row">
+        <div class="col-md-12">
+              <!-- Start Main View -->
+            @if(session()->has('message'))
+                <div class="alert alert-success alert-dismissible fade show">
+                  <strong>Success!</strong> {{ session()->get('message') }}
+                  <button type="button" class="close" data-dismiss="alert">&times;</button>
+              </div>
+            @endif
+
+             @if ($errors->any())
+               <div class="alert alert-warning alert-dismissible fade show">
+                 <button type="button" class="close" data-dismiss="alert">&times;</button>
+                  <strong>Error!</strong>  
+                   {{implode(',',$errors->all() )}}
+                </div>
+             @endif
+
+            <div class="card-body">
+                <div class="row mb-2">
+                    <div class="col-6">
+                        <h4 class="mt-0 text-left">Inspection Type List</h4>
+                    </div>
+                    <div class="col-6 text-right">
+                        <button type="button" class="btn btn-danger mt-0"  onclick="return window.location.href='inspection-types/create'">Add Inspection Type
+                        </button>
+                    </div>
+                </div>
+                <!-- Categories Table -->
+                <div class="table-responsive">
+                    <table id="project-types-table" class="table table-hover text-center">
+                        <thead>
+                        <tr class="text-danger">
+                            <th>Acc. No.</th>
+                            <th>Inspetion Type</th>
+                            <!-- <th>Projects</th> -->
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                          @foreach($inspetionTypes as $type)
+                         <tr>
+                           <td> {{ $type->account_number }}</td>
+                           <td>{{ $type->name }}</td>
+                           <td>        
+                            <button onclick="return window.location.href='inspection-types/{{$type->id}}'" rel="tooltip" class="btn btn-neutral bg-transparent btn-icon" data-original-title="Edit Inspection Type" title="Edit Inspection Type">            <i class="fa fa-edit text-success"></i>        </button> 
+                          </td>
+                          <td>
+                             <form 
+                              method="post" 
+                              action="{{route('inspection-types.destroy',$type->id)}}"> 
+                               @csrf
+                              {{ method_field('DELETE') }}
+
+                              <button 
+                                type="submit"
+                                onclick="return confirm('Are you sure?')"
+                                class="btn btn-neutral bg-transparent btn-icon" data-original-title="Delete Inspection Type" title="Delete Inspection Type"><i class="fa fa-trash text-danger"></i> </button>
+                            </form>
+                           </td>
+                         </tr> 
+                         @endforeach
+                        <!-- Project Types Go Here -->
+                        </tbody>
+                    </table>
+                </div>
+                 {!! $inspetionTypes->render() !!}
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
