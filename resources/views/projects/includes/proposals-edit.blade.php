62d9eadf341fac2bc11d0caa9ff682c1883dbd9d
Kamlesh <dhundharakamlesh@gmail.com>
2022-10-22 07:32:50 +0530

    WIP

--- resources/views/projects/includes/proposals-edit.blade.php
+++ resources/views/projects/includes/proposals-edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 @include('includes.back', 
-['url' => route("projects.show", ['project' => request()->id]),
+['url' => route("projects.show", ['project' => @$proposal->project_id]),
 'to' => 'to Project'])
 
 
